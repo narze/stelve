c95b74df9791dfb7f0e8a7f3d22d4f60b89a11e3
Manassarn Manoonchai <manassarn@gmail.com>
2022-09-19 02:36:09 +0700

    Add list of components (wip)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="min-h-[calc(100vh-4rem)] grid place-items-center">
+<div class="min-h-[calc(100vh-4rem)] flex flex-col items-center justify-center gap-6 p-8">
 	<div class="hero max-w-5xl p-8 bg-base-200">
 		<div class="hero-content text-center">
 			<div class="max-w-xl">
@@ -7,6 +7,17 @@
 					Visit <a class="link" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 				</p>
 			</div>
+		</div>
+	</div>
+
+	<div class="card bg-base-200 max-w-5xl w-full rounded-none">
+		<div class="card-body">
+			<h2 class="card-title">Components</h2>
+
+			<ul>
+				<li><a href="#" class="link">Clock (TODO)</a></li>
+				<li><a href="#" class="link">Stopwatch (TODO)</a></li>
+			</ul>
 		</div>
 	</div>
 </div>
