603c6804c6a7e3927094559be04d7d6ae9135c38
Manassarn Manoonchai <manassarn@gmail.com>
2022-09-19 01:55:43 +0700

    Theme changer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script>
   import "../app.postcss";
+  import { onMount } from "svelte";
+  import { themeChange } from 'theme-change'
+
+  onMount(() => {
+    themeChange(false)
+    // 👆 false parameter is required for svelte
+  })
 </script>
 
 <div class="flex flex-col min-h-screen">
   <header class="navbar bg-neutral text-neutral-content overflow-clip">
-    <span class="normal-case text-xl px-4 font-bold">Stelve</span>
-    <span class="normal-case text-xl px-2 font-light whitespace-nowrap">A SvelteKit playground</span>
+    <div class="flex-1">
+      <span class="normal-case text-xl px-4 font-bold">Stelve</span>
+      <span class="normal-case text-xl px-2 font-light whitespace-nowrap">A SvelteKit playground</span>
+    </div>
+    <div class="flex-none">
+      🌞<input type="checkbox" data-toggle-theme="dark,light" data-act-class="ACTIVE" class="toggle mx-2" />🌝
+    </div>
   </header>
 
   <main class="container mx-auto flex-grow">
